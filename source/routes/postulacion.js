d20001158af83e831849556ffc54267c79334fe4
Santixgo-bit <ivancharris0@gmail.com>
2025-11-01 14:29:15 -0500

    v0.0.3.3 creacion rutas postulaciones

---
+++ source/routes/postulacion.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Postulacion = require("../models/Postulacion");
+
+router.post("/applications", async (req, res) => {
+  try {
+    const postulacion = new Postulacion(req.body);
+    const guardada = await postulacion.save();
+    res.status(201).json(guardada);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
