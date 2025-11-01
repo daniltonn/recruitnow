5b1b9beec8285c784da59295ae4ed6600ded1057
CristianH5 <cristian2000amado@gmail.com>
2025-11-01 12:30:11 -0500

    v0.2.2.2 agregar validaciones modelo usuario

--- source/model/usuarioModel.js
+++ source/model/usuarioModel.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 
 const usuarioSchema = new mongoose.Schema({
-  nombre: String,
-  email: String,
-  contraseña: String
+  nombre: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  contraseña: {
+    type: String,
+    required: true
+  },
+  rol: {
+    type: String,
+    enum: ["Candidato", "Reclutador", "Administrador"],
+    default: "Candidato"
+  },
+  fechaRegistro: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("Usuario", usuarioSchema);
